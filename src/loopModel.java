faf49be800ff476bbf744e07c0376d1e3425588c
gocersensei <gocersensei@gmail.com>
2020-12-19 18:01:55 +0000

    This program is an example of an "if" within a  "do while" loop

---
+++ src/loopModel.java
@@ -0,0 +1,21 @@
+public class loopModel {
+
+    public static void main(String[] args) {
+        int i = 0;
+        int number=7;
+
+        do {
+            System.out.println(number);
+            i++;
+            if(i==1)
+            {
+                System.out.println("Middle of the loop");
+            }
+        }
+        while (i<2);
+
+
+
+    }
+
+}
